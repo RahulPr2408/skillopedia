e8a27d742526099212d7507181063a4d00ae9c22
RahulPr2408 <prajapati.rahu201@gmail.com>
2022-12-01 22:55:29 +0530

    profile screen is done

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,10 +1,71 @@
-import React from 'react'
-import "./Profile.css"
+import React from "react";
+import "./Profile.css";
+import background from "../assests/background.jpg";
+import person from "../assests/person.jpg";
+import { AiOutlineEdit } from "react-icons/ai";
 
 const Profile = () => {
-    return (
-        <div>Profile</div>
-    )
-}
+  return (
+    <div className="profile_container">
+      <div className="image">
+        <img src={background} alt="" />
+      </div>
+      <div className="info_header">
+        <img src={person} alt="" />
+        <div className="details">
+          <h5>Rahul Prajapati</h5>
+          <span>abc@gmail.com</span>
+        </div>
+      </div>
+      <div className="main_info_container">
+        <div className="profile_info_form">
+          <div className="profile_info_form_header">
+            <h4>Profile Information</h4>
+            <AiOutlineEdit className="profile_info_form_header_edit" />
+          </div>
+          <div className="main_form">
+            <label htmlFor="firstname">First Name</label>
+            <input type="text" name="firstname" />
+            <label htmlFor="lastname">Last Name</label>
+            <input type="text" name="lastname" />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+            <label htmlFor="totalExperience">Total Experience</label>
+            <input type="email" name="totalExperience" />
+            <label htmlFor="Role">Role</label>
+            <input type="email" name="Role" />
+            <label htmlFor="currentProject">Current Project</label>
+            <input type="email" name="currentProject" />
+          </div>
+        </div>
+        <div className="skill_set_info">
+          <div className="skill_set_info_header">
+            <h4>Skill Set</h4>
+            <AiOutlineEdit className="skill_set_info_header_edit" />
+          </div>
+          <div className="skill_set_info_main_form">
+            <div>
+              <label htmlFor="skill">Skill</label>
+              <input type="text" name="skill" />
+            </div>
+            <div>
+              <label htmlFor="experience">Experience</label>
+              <input type="text" name="experience" />
+            </div>
+            <div>
+              <label htmlFor="github_link">GitHub Link</label>
+              <input type="text" name="github_link" />
+            </div>
+          </div>
+          <span className="add_skill">+ Add Skill</span>
+        </div>
+      </div>
+      <div className="button_container">
+        <button className="btn_save">SAVE</button>
+        <button className="btn_cancel">CANCEL</button>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,27 +1,41 @@
-import React from 'react'
-import "./Sidebar.css"
-import { Link } from "react-router-dom"
-import { AiFillHome } from "react-icons/ai"
-import { MdOutlineVerified } from "react-icons/md"
-import { AiOutlineUser } from "react-icons/ai"
-import { useGlobalContext } from '../util/Context'
+import React from "react";
+import "./Sidebar.css";
+import { Link } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { MdOutlineVerified } from "react-icons/md";
+import { AiOutlineUser } from "react-icons/ai";
+import { useGlobalContext } from "../util/Context";
+import { HiLogout } from "react-icons/hi";
 
 const Sidebar = () => {
+  const { showProfile, showAdmin, showDashboard } = useGlobalContext();
 
-    const { showProfile, showAdmin, showDashboard } = useGlobalContext()
+  return (
+    <div className="sidebar_container">
+      <div className="sidebar_content">
+        <h3>SKILLOPEDIA</h3>
+        <ul>
+          <li onClick={showDashboard}>
+            <AiFillHome className="icons" />
+            <Link to="/skillopedia/admindashboard">Dashboard</Link>
+          </li>
+          <li onClick={showAdmin}>
+            <MdOutlineVerified className="icons" />
+            <Link to="/skillopedia/adminpendingrequest">Admin</Link>
+          </li>
+          <li onClick={showProfile}>
+            <AiOutlineUser className="icons" />
+            <Link to="/skillopedia/profile">Profile</Link>
+          </li>
+        </ul>
+        <ul className="logout">
+          <li>
+            Logout <HiLogout />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className='sidebar_container'>
-            <div className='sidebar_content'>
-                <h3>SKILLOPEDIA</h3>
-                <ul>
-                    <li onClick={showDashboard}><AiFillHome className='icons' /><Link to="/skillopedia/admindashboard">Dashboard</Link></li>
-                    <li onClick={showAdmin}><MdOutlineVerified className='icons' /><Link to="/skillopedia/adminpendingrequest">Admin</Link></li>
-                    <li onClick={showProfile}><AiOutlineUser className='icons' /><Link to="/skillopedia/profile">Profile</Link></li>
-                </ul>
-            </div>
-        </div>
-    )
-}
-
-export default Sidebar
+export default Sidebar;
